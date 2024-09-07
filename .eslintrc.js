5077d7ef009704f96dd2e749cffba4c4dbb04fa2
eyob721 <eyobmelkamu721@gmail.com>
2024-09-07 12:27:05 +0300

    feat: initialize npm module and setup eslint semi-standard config

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    commonjs: true
+  },
+  extends: ['eslint:recommended', 'standard'],
+  rules: {
+    semi: ['off', 'always']
+  }
+};
